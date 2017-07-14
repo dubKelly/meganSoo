d66b10bef0f5c169687b5277f6c450c82905283d
dubKelly <js.neeb1780@gmail.com>
2017-07-14 18:45:37 -0400

    ...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,41 +1,43 @@
-(function landFocus() {
-	var landFocus = document.getElementsByClassName("landFocus");
+// (function landFocus() {
+// 	var landFocus = document.getElementsByClassName("landFocus");
+// 	document.onmousemove = function mouseX(event) {
+// 		if (event.clientX < (window.innerWidth / 3)) {
+// 			landFocus[0].classList.add("focus");
+// 			landFocus[1].classList.add("focus");
+// 			landFocus[2].classList.add("mFocus");
+// 			landFocus[3].classList.add("outFocus");
+// 		}
+// 		else if (event.clientX > (window.innerWidth * 0.66)) {
+// 			landFocus[0].classList.add("outFocus");
+// 			landFocus[2].classList.add("sFocus");
+// 			landFocus[3].classList.add("focus");
+// 			landFocus[4].classList.add("focus");
+// 		}
+// 		else {
+// 			landFocus[0].classList.remove("focus");
+// 			landFocus[0].classList.remove("outFocus");
+// 			landFocus[1].classList.remove("focus");
+// 			landFocus[2].classList.remove("mFocus");
+// 			landFocus[2].classList.remove("sFocus");
+// 			landFocus[3].classList.remove("focus");
+// 			landFocus[3].classList.remove("outFocus");
+// 			landFocus[4].classList.remove("focus");
+// 		}
+// 	}
+// })();
+(function focus() {
+	var focus = document.getElementsByClassName("focus");
 	document.onmousemove = function mouseX(event) {
-		if (event.clientX < (window.innerWidth / 3)) {
-			landFocus[0].classList.add("focus");
-			landFocus[1].classList.add("focus");
-			landFocus[2].classList.add("mFocus");
-			landFocus[3].classList.add("outFocus");
-		}
-		else if (event.clientX > (window.innerWidth * 0.66)) {
-			landFocus[0].classList.add("outFocus");
-			landFocus[2].classList.add("sFocus");
-			landFocus[3].classList.add("focus");
-			landFocus[4].classList.add("focus");
-		}
-		else {
-			landFocus[0].classList.remove("focus");
-			landFocus[0].classList.remove("outFocus");
-			landFocus[1].classList.remove("focus");
-			landFocus[2].classList.remove("mFocus");
-			landFocus[2].classList.remove("sFocus");
-			landFocus[3].classList.remove("focus");
-			landFocus[3].classList.remove("outFocus");
-			landFocus[4].classList.remove("focus");
+		for (var i = focus.length - 1; i >= 0; i--) {
+			if (event.clientX < (window.innerWidth / 3)) {
+				focus[i].classList.add("mFocus");
+			}
+			else if (event.clientX > (window.innerWidth * 0.66)) {
+				focus[i].classList.add("sFocus");
+			}
+			else {
+				focus[i].classList.remove("mFocus", "sFocus");
+			}
 		}
 	}
-})();
-(function dHead() {
-	var deskHead = document.getElementsByClassName("deskHead");
-	deskHead[4].onmouseenter = function deskEnter() {
-		for (var i = deskHead.length - 1; i >= 0; i--) {
-			deskHead[i].classList.add("hvr");
-			console.log("hover");
-		};
-	}
-	deskHead[4].onmouseleave = function deskLeave() {
-		for (var i = deskHead.length - 1; i >= 0; i--) {
-			deskHead[i].classList.remove("hvr");
-		};
-	};
 })();
