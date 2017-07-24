b7cc4396c13c2ef066f38b91576618493bb597d2
dubKelly <js.neeb1780@gmail.com>
2017-07-24 03:42:57 -0400

    smoothScroll

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,45 @@
 })();
 
 (function menuHover() {
-	$(".portHead").hover(function() {
-		$(".portHead").not(this).toggleClass("fade");
+	var portHead = $(".portHead");
+	$(portHead).hover(function() {
+		if (portHead[0].classList.contains("open") == true) {
+			$(portHead).not(this).toggleClass("fade");
+		}
 	})
 })();
+function filterPath(string) {
+  return string
+    .replace(/^\//, '')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
+    .replace(/\/$/, '');
+}
 
+var locationPath = filterPath(location.pathname);
+$('a[href*="#"]').each(function () {
+  var thisPath = filterPath(this.pathname) || locationPath;
+  var hash = this.hash;
+  if ($("#" + hash.replace(/#/, '')).length) {
+    if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
+      var $target = $(hash), target = this.hash;
+      if (target) {
+        $(this).click(function (event) {
+          event.preventDefault();
+          $('html, body').animate({scrollTop: $target.offset().top}, 1000, function () {
+            location.hash = target; 
+            $target.focus();
+            if ($target.is(":focus")){ //checking if the target was focused
+              return false;
+            }else{
+              $target.attr('tabindex','-1'); //Adding tabindex for elements not focusable
+              $target.focus(); //Setting focus
+            };
+          });       
+        });
+      }
+    }
+  }
+});
 // (function focus() {
 // 	var focus = document.getElementsByClassName("focus");
 // 	document.onmousemove = function mouseX(event) {
