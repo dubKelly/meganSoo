2f8756780c57d5f205f989fb565b326cce48d8a7
dubKelly <js.neeb1780@gmail.com>
2017-07-25 17:16:53 -0400

    type script

--- js/scripts.js
+++ js/scripts.js
@@ -16,41 +16,66 @@
 		}
 	})
 })();
-function filterPath(string) {
-  return string
-    .replace(/^\//, '')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
-    .replace(/\/$/, '');
-}
 
-var locationPath = filterPath(location.pathname);
-$('a[href*="#"]').each(function () {
-  var thisPath = filterPath(this.pathname) || locationPath;
-  var hash = this.hash;
-  if ($("#" + hash.replace(/#/, '')).length) {
-    if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
-      var $target = $(hash), target = this.hash;
-      if (target) {
-        $(this).click(function (event) {
-          console.log(this);
-          console.log(thisPath);
-          console.log(hash);
-          event.preventDefault();
-          $('html, body').animate({scrollTop: $target.offset().top}, 1000, function () {
-            location.hash = target; 
-            $target.focus();
-            if ($target.is(":focus")){ //checking if the target was focused
-              return false;
-            }else{
-              $target.attr('tabindex','-1'); //Adding tabindex for elements not focusable
-              $target.focus(); //Setting focus
-            };
-          });       
-        });
+(function smoothScroll() {
+  function filterPath(string) {
+    return string
+      .replace(/^\//, '')
+      .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
+      .replace(/\/$/, '');
+  }
+  var locationPath = filterPath(location.pathname);
+  $('a[href*="#"]').each(function () {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    var hash = this.hash;
+    if ($("#" + hash.replace(/#/, '')).length) {
+      if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
+        var $target = $(hash), target = this.hash;
+        if (target) {
+          $(this).click(function (event) {
+            console.log(this);
+            console.log(thisPath);
+            console.log(hash);
+            event.preventDefault();
+            $('html, body').animate({scrollTop: $target.offset().top}, 1000, function () {
+              location.hash = target; 
+              $target.focus();
+              if ($target.is(":focus")){ //checking if the target was focused
+                return false;
+              }else{
+                $target.attr('tabindex','-1'); //Adding tabindex for elements not focusable
+                $target.focus(); //Setting focus
+              };
+            });       
+          });
+        }
+      }
+    }
+  });
+})();
+
+(function categoryFilter() {
+  var categories = document.getElementsByClassName("categories");
+  var gallery = document.getElementsByClassName("gallery");
+  var type = "";
+  for (var i = categories.length - 1; i >= 0; i--) {
+    categories[i].onclick = function() {
+      function hasClass(element, cls) {
+        return ('' + element.className + '').indexOf('' + cls + '') > -1; 
+      }
+      type = this.getAttribute("data-type");
+      for (var i = gallery.length - 1; i >= 0; i--) {
+        if (hasClass(gallery[i], type) === false) {
+          console.log(gallery[i]);
+          gallery[i].classList.add("hide");
+        }
+        else {
+          gallery[i].classList.remove("hide");
+        }
       }
     }
   }
-});
+})();
 // (function focus() {
 // 	var focus = document.getElementsByClassName("focus");
 // 	document.onmousemove = function mouseX(event) {
