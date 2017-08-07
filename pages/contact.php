acdc525f9d1d915bb036252ecf09c6ad6f1136a5
dubKelly <js.neeb1780@gmail.com>
2017-08-07 11:24:43 -0400

    footer1060

--- pages/contact.php
+++ pages/contact.php
@@ -155,6 +155,7 @@ $newToken = generateFormToken('form1');
 			</style><g class="st2" id="layer_copy"><g class="st0"><polygon class="st1" points="256,62 473,279 256,62 39,279   "/><path class="st1" d="M414,295v155H98V295"/></g></g><g id="layer_copy_2"><g><path d="M473,287c-2.048,0-4.095-0.781-5.657-2.343L256,73.313L44.657,284.657c-3.124,3.123-8.189,3.123-11.313,0    C31.781,283.095,31,281.048,31,279s0.781-4.095,2.343-5.657l217-217c3.125-3.124,8.19-3.123,11.314,0l217,217    c1.562,1.563,2.343,3.609,2.343,5.657s-0.781,4.095-2.343,5.657C477.095,286.219,475.048,287,473,287z"/></g><g><path d="M414,458H98c-4.418,0-8-3.582-8-8V295c0-4.418,3.582-8,8-8s8,3.582,8,8v147h300V295c0-4.418,3.582-8,8-8s8,3.582,8,8v155    C422,454.418,418.418,458,414,458z"/></g></g></svg>
 		</a>
 	</div>
+	<span class="footPart left"></span>
 	<div class="icons footIcons">
 		<a class="facebook" href="https://web.facebook.com/profile.php?id=1049235787">
 			<svg class="icon" height="1.70667in" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" version="1.1" viewBox="0 0 8379 8379" width="1.70667in" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><style type="text/css">
@@ -188,6 +189,13 @@ $newToken = generateFormToken('form1');
 		   ]]>
 		  </style></defs><g id="Layer_x0020_1"><rect class="fil0" height="7630" width="7630"/><g id="_899316136"><g><path class="fil1 you" d="M4035 3421c58,0 137,-30 219,-127l0 112 190 0 0 -1019 -190 0 0 774c-23,29 -74,76 -111,76 -41,0 -51,-28 -51,-68l0 -782 -189 0 0 852c0,101 31,182 132,182z"/><path class="fil1 you" d="M3192 3152c0,180 94,273 278,273 152,0 272,-102 272,-273l0 -499c0,-160 -119,-274 -272,-274 -167,0 -278,110 -278,274l0 499zm195 -483l0 0c0,-55 26,-97 79,-97 58,0 83,40 83,97l0 475c0,55 -28,96 -79,96 -53,0 -83,-43 -83,-96l0 -475z"/><polygon class="fil1 you" points="2740,3406 2953,3406 2953,2846 3202,2025 2985,2025 2848,2577 2701,2025 2486,2025 2740,2846 "/></g><g><path class="fil1 tube" d="M4699 3638l-2234 0c-240,0 -434,195 -434,435l0 1098c0,240 194,434 434,434l2234 0c240,0 434,-194 434,-434l0 -1098c0,-240 -194,-435 -434,-435zm-1936 1642l0 0 -203 0 0 -1125 -211 0 0 -191 624 0 0 191 -210 0 0 1125zm723 0l0 0 -181 0 0 -107c-33,39 -68,69 -103,90 -98,56 -231,55 -231,-143l0 -811 180 0 0 744c0,39 10,66 48,66 35,0 84,-45 106,-73l0 -737 181 0 0 971zm695 -201l0 0c0,120 -45,213 -165,213 -66,0 -122,-24 -172,-87l0 75 -182 0 0 -1316 182 0 0 424c41,-50 96,-91 161,-91 132,0 176,112 176,243l0 539zm667 -262l0 0 -345 0 0 183c0,73 6,136 78,136 77,0 81,-51 81,-136l0 -67 186 0 0 73c0,187 -81,300 -271,300 -172,0 -260,-125 -260,-300l0 -436c0,-168 111,-285 274,-285 173,0 257,110 257,285l0 247z"/><path class="fil1 tube" d="M4583 4455c-67,0 -80,47 -80,113l0 99 159 0 0 -99c0,-65 -14,-113 -79,-113z"/><path class="fil1 tube" d="M3881 4461c-12,6 -25,16 -37,29l0 605c15,16 29,27 43,34 29,15 72,16 92,-10 11,-13 16,-36 16,-67l0 -501c0,-33 -7,-58 -19,-74 -22,-29 -63,-32 -95,-16z"/></g></g></g></svg>
 	  </a>
+	</div>
+	<span class="footPart right"></span>
+	<div class="footNavCont">
+		<a href="#motion"><span class="footNav footMotion">Motion</span></a>
+		<a href="#stills"><span class="footNav footStills">Stills</span></a>
+		<a href="#about"><span class="footNav footAbout">About</span></a>
+		<a href="pages/contact.php"><span class="footNav footContact">Contact</span></a>
 	</div>
 	<p class="copyright">&#169 2017 Megan Soo</p>
 </div>
